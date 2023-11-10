aeb2ce7068458878a0e4c008f731c11edc03d91a
Yuyan <songyuyan2001@hotmail.com>
2023-11-09 17:47:06 -0800

    lc helper function header

---
+++ lc_helper_func.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+void printVectorInline(std::vector<T>& v) {
+  for (auto& d : v) {
+    std::cout << d << "  ";
+  }
+  std::cout << std::endl;
+}
